430f8dbe3df9ff64f1a758a57754ea394fdf38ef
Marek Pietrucha <marek@enginearch.com>
2017-01-20 14:40:53 +0100

    added buttons support to lists

--- lib/messenger/components/templates/compact_list.rb
+++ lib/messenger/components/templates/compact_list.rb
@@ -5,15 +5,16 @@ module Messenger
     class CompactList
       include Components::Attachment
 
-      attr_accessor :template_type, :elements, :top_element_style
+      attr_accessor :template_type, :elements, :top_element_style, :buttons
 
-      ATTRIBUTES = %w(template_type elements top_element_style).freeze
+      ATTRIBUTES = %w(template_type elements top_element_style buttons).freeze
 
-      def initialize(elements:)
+      def initialize(elements:, buttons:)
         @type               = 'template'
         @template_type      = 'list'
         @top_element_style  = 'compact'
         @elements           = build_elements(elements)
+        @buttons            = build_elements(buttons)
       end
     end
   end
--- lib/messenger/components/templates/list.rb
+++ lib/messenger/components/templates/list.rb
@@ -5,14 +5,15 @@ module Messenger
     class List
       include Components::Attachment
 
-      attr_accessor :template_type, :elements
+      attr_accessor :template_type, :elements, :buttons
 
-      ATTRIBUTES = %w(template_type elements).freeze
+      ATTRIBUTES = %w(template_type elements, buttons).freeze
 
-      def initialize(elements:)
+      def initialize(elements:, buttons:)
         @type               = 'template'
         @template_type      = 'list'
         @elements           = build_elements(elements)
+        @buttons            = build_elements(buttons)
       end
     end
   end
