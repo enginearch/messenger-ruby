209ae3a5a2805de9a2859403ee7e557d2a13df5f
Marek Pietrucha <marek@enginearch.com>
2017-01-24 15:37:28 +0100

    ..

--- lib/messenger/parameters/messaging.rb
+++ lib/messenger/parameters/messaging.rb
@@ -3,7 +3,7 @@ module Messenger
     class Messaging
       attr_accessor :sender_id, :recipient_id, :callback
 
-      def initialize(sender:, recipient:, timestamp: nil, message: nil, delivery: nil, postback: nil, optin: nil, read: nil, account_linking: nil, referral:)
+      def initialize(sender:, recipient:, timestamp: nil, message: nil, delivery: nil, postback: nil, optin: nil, read: nil, account_linking: nil, referral: nil)
         @sender_id    = sender['id']
         @recipient_id = recipient['id']
         @callback     = set_callback(message: message, delivery: delivery, postback: postback, optin: optin, read: read, account_linking: account_linking, referral: referral)
